727e47d8f4dcc1247cee61af2b120c0cb97c3344
Daniel <hello@danmatthew.co.uk>
2013-04-22 11:37:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>Menu</title>
+	<meta charset="utf-8">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<h1>Menu</h1>
+	<dl>
+		<dt>Monday:</dt>
+		<dd>Curry(?)</dd>
+		<dt>Tuesday:</dt>
+		<dd>Meatballs (v)</dd>
+		<dt>Wednesday:</dt>
+		<dd>Meat Night</dd>
+		<dt>Thursday:</dt>
+		<dd>Gnocchi with mustard, cider, and cream</dd>
+		<dt>Friday:</dt>
+		<dd>???</dd>
+	</dl>
+</body>
+<script type="text/javascript">
+  (function() {
+    var config = {
+      kitId: 'fjc7tnf',
+      scriptTimeout: 3000
+    };
+    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+  })();
+</script>
+</html>
